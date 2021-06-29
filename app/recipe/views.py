b0ef9efd8a3ab76e98a393856f6c4c67d5896307
Don Windrem <dwindrem@gmail.com>
2021-06-29 17:23:38 -0400

    added tag support to recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -40,3 +40,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
